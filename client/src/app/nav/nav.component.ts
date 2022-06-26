d525263ade3e39f9040c91bc17ca54ec80dda3cb
Pawel <pawelmusial1995@gmail.com>
2022-06-26 14:26:44 +0200

    Add Auth Guard + Refactor App module

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -28,7 +28,7 @@ export class NavComponent implements OnInit {
       this.router.navigateByUrl('/members');
     },error => {
       console.log(error);
-      this.toastr.error(error.error);
+      this.toastr.error(error.error.title);
     }); 
   }
 
