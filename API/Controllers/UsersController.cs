82889a459698fec424a4713c6735b35a8b27b4aa
Pawel <pawelmusial1995@gmail.com>
2022-06-27 21:35:23 +0200

    Adding seed data to API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,7 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
+{ 
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -14,16 +15,13 @@ namespace API.Controllers
             _context = context;
         }
 
-
         [HttpGet]
-        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers ()
         {
             return Ok(await _context.Users.ToListAsync());
         }
 
         //api/users/3
-        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers (int id)
         {
